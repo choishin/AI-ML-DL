97be77b5b955c9e3c2b81e2e0df52499dcca9964
choishin <choishin@gmail.com>
2021-07-26 22:00:57 +0900

    clean

--- Grammar/constant.py
+++
@@ -1,3 +0,0 @@
-import tensorflow as tf
-a = tf.constant([10],dtype=tf.float32)
-print(a)
--- HelloWorld/test2.py
+++
@@ -1,8 +0,0 @@
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-result = tf.constant("Tensorflow is easy!!")
-sess = tf.Session()
-print(str(sess.run(result),encoding='utf8'))
